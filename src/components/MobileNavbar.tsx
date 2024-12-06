55407d3ae7acb04b0d80d0198cfc6e60ab6ff233
mA4rK0 <onlymarko25@gmail.com>
2024-12-07 00:27:00 +0700

    give a toggle navigation bar menu a aria-label for screenreader.

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -2,7 +2,7 @@ export default function MobileNavbar({ state, isBorder }) {
   return (
     <>
       <section className={`flex items-center ${isBorder && "border px-2 rounded"}`}>
-        <button onClick={state}>
+        <button aria-label="toggle navigation menu" onClick={state}>
           <span className="w-[30px] h-[2px] my-2 block bg-lightBlue"></span>
           <span className="w-[30px] h-[2px] my-2 block bg-lightBlue"></span>
           <span className="w-[30px] h-[2px] my-2 block bg-lightBlue"></span>
